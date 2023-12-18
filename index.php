a554737c342e1ea987601d10b4844db371a26b4a
Rayan Outili <rayan.outili@etu.unice.fr>
2023-12-18 11:41:31 +0100

    msg template style

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./public/views/css/main.css">    
+    <link rel="stylesheet" href="./public/views/css/message.css">    
     <title>Réseau social</title>
 </head>
 <body>
@@ -13,7 +15,6 @@
 
 <?php
 
-    require_once("public/views/php/MainView.php");
-
+    require_once("public/views/php/message.php");
 
 ?>
--- public/views/php/message.php
+++ public/views/php/message.php
@@ -1,11 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+    <div class="box-message">
+
+        <div class="left-message">
+            <div class="send-to">
+                <p>Envoyer un message : </p>
+            </div>
+
+            <div class="contact">
+                <div class="discussion-item">
+                    <p>Discussion 1</p>
+                </div>
+                <div class="discussion-item">
+                    <p>Discussion 1</p>
+                </div>
+                <div class="discussion-item">
+                    <p>Discussion 1</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="right-message">
+            <div class="top-message">
+                <p>Discussion 1</p>
+            </div>
+            <p>eiohjfoejfejzfbezj</p>é
+        </div>
+
+
+
+    </div>
 </body>
+
 </html>
