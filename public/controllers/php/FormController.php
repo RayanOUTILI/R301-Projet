62b63970a58877867b6e456176afab661f92a651
Thomas Portelette <thomas.portelette@etu.unice.fr>
2023-12-27 21:25:28 +0100

    oubli

---
+++ public/controllers/php/FormController.php
@@ -0,0 +1,21 @@
+<?php
+
+require __DIR__ . "/Controller.php";
+
+class FormController extends Controller
+{
+    private $formView;
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    
+
+   
+
+
+}
+
+
+
+?>
